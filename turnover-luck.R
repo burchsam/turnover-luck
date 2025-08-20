69e0ca31470227901948479d8409ec17f1d85464
Sam Burch <73138593+burchsam@users.noreply.github.com>
2025-08-20 17:28:37 -0400

    Add files via upload

--- turnover-luck.R
+++ turnover-luck.R
@@ -5,30 +5,15 @@ library(nflreadr)
 # Creating Turnover Luck Dataset ------------------------------------------
 
 
-# pbp_tot = load_pbp(1999:2023)
-# 
-# pbp_23 |> 
-#   filter(season == 2023) |> 
-#   filter(!is.na(yards_gained), (pass == 1 |  rush == 1), (interception == 1 | fumble_forced == 1 | sack == 1)) |> 
-#   group_by(posteam) |> 
-#   summarise(avg_to = mean(epa),
-#             tot_to = sum(epa),
-#             fum_lost_rate = sum(fumble_lost == 1) / sum(fumble_forced == 1),
-#             ints = sum(interception),
-#             fumbles = sum(fumble_forced),
-#             sacks = sum(sack == 1),
-#             tos = n(),
-#             sacks_pct = sacks / tos,
-#             .groups = "drop") |> 
-#   arrange(tot_to)
+# pbp_tot = load_pbp(1999:2024)
 # 
 # 
 # 
 # 
 # # Offense TO
-# off_to = pbp_tot |> 
-#   filter(!is.na(yards_gained), (pass == 1 |  rush == 1), (interception == 1 | fumble_forced == 1 | sack == 1)) |> 
-#   group_by(posteam, season) |> 
+# off_to = pbp_tot |>
+#   filter(!is.na(yards_gained), (pass == 1 |  rush == 1), (interception == 1 | fumble_forced == 1 | sack == 1)) |>
+#   group_by(posteam, season) |>
 #   summarise(avg_to_epa = mean(epa),
 #             tot_to_epa = sum(epa),
 #             fum_lost_rate = sum(fumble_lost == 1) / sum(fumble_forced == 1),
@@ -37,13 +22,13 @@ library(nflreadr)
 #             sacks = sum(sack == 1 & fumble_forced != 1),
 #             tos = n(),
 #             sacks_pct = sacks / tos,
-#             .groups = "drop") |> 
+#             .groups = "drop") |>
 #   arrange(tot_to_epa)
 # 
 # # Defense TO
-# def_to = pbp_tot |> 
-#   filter(!is.na(yards_gained), (pass == 1 |  rush == 1), (interception == 1 | fumble_forced == 1 | sack == 1)) |> 
-#   group_by(defteam, season) |> 
+# def_to = pbp_tot |>
+#   filter(!is.na(yards_gained), (pass == 1 |  rush == 1), (interception == 1 | fumble_forced == 1 | sack == 1)) |>
+#   group_by(defteam, season) |>
 #   summarise(avg_to_forced_epa = mean(epa),
 #             tot_to_forced_epa = sum(epa),
 #             fum_lost_rate_forced = sum(fumble_lost == 1) / sum(fumble_forced == 1),
@@ -52,18 +37,18 @@ library(nflreadr)
 #             sacks_forced = sum(sack == 1 & fumble_forced != 1),
 #             tos_forced = n(),
 #             sacks_forced_pct = sacks_forced / tos_forced,
-#             .groups = "drop") |> 
+#             .groups = "drop") |>
 #   arrange(-tot_to_forced_epa)
-#
+# 
 # to_df = off_to |>
-#   left_join(def_to, by = c("posteam" = "defteam", "season")) |> 
-#   rename(team = posteam) |> 
+#   left_join(def_to, by = c("posteam" = "defteam", "season")) |>
+#   rename(team = posteam) |>
 #   mutate(net_tos = tos - tos_forced,
 #          net_to_epa_tot = tot_to_epa - tot_to_forced_epa,
-#          net_to_epa_avg = net_to_epa_tot / (tos + tos_forced)) |> 
+#          net_to_epa_avg = net_to_epa_tot / (tos + tos_forced)) |>
 #   arrange(-net_to_epa_tot)
-
-
+# 
+# 
 # write.csv(to_df, file = "turnover-value.csv")
 
 
@@ -75,21 +60,21 @@ library(nflreadr)
 to_df = read_csv("coding-projects/nfl-fast-r/turnover-value.csv")[, -1]
 
 
-to_df |> filter(season == 2023) |> colnames()
+to_df |> filter(season == 2024) |> colnames()
 
 
 
 # Total "Turnover" Impact
 
-ggplot(to_df |> filter(season == 2023), 
+ggplot(to_df |> filter(season == 2024), 
        aes(x = net_to_epa_tot, y = reorder(team, net_to_epa_tot))) +
   labs(
-    title = "NFL Drive Killer Luck (2023)",
+    title = "NFL Drive Killer Luck (2024)",
     subtitle = "drive killer = sack, interception, or fumble forced",
     caption = "By: Sam Burch  |  Data @nflfastR",
     x = "Total EPA on Drive Killers"
   ) +
-  # scale_x_continuous(breaks = seq(-200, 200, 50)) +
+  scale_x_continuous(breaks = seq(-200, 200, 50)) +
   geom_col(aes(color = team, fill = team), alpha = .8, width = 1) +
   nflplotR::scale_fill_nfl(type = "primary") +
   nflplotR::scale_color_nfl(type = "secondary") +
@@ -107,7 +92,7 @@ ggplot(to_df |> filter(season == 2023),
   ) +
   nflplotR::geom_nfl_logos(aes(team_abbr = team), width = .03, alpha = .8)
 
-# ggsave("nfl-dk-luck-23.png", width = 16, height = 12, units = "cm")
+# ggsave("nfl-dk-luck.png", width = 16, height = 12, units = "cm")
 
 
 
